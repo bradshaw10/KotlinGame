86e21090f4d6af5672f8d693c39d4ba8892f2c80
bradshaw10 <eoghan.bradshaw@gmail.com>
2022-01-05 22:59:18 +0000

    dracula king added

--- KotlinGame/app/src/main/java/Main.kt
+++ KotlinGame/app/src/main/java/Main.kt
@@ -1,52 +1,33 @@
 fun main(args: Array<String>) {
-    val tim = Player("Tim")
+    val enemy = Enemy("test_enemy", 10, 3)
+    println(enemy)
 
-    tim.show()
+    enemy.takeDamage(11)
+    println(enemy)
 
-    val louise = Player("louise", 5)
-    louise.show()
-    val eoghan = Player("eoghan", 4, 8)
-    eoghan.show()
+    val uglyTroll = Troll("UglyTroll")
+    println(uglyTroll)
+    uglyTroll.takeDamage(30)
+    println(uglyTroll)
 
-    val erica = Player("erica", 2, 3, 1000)
-    erica.show()
+    val vlad = Vampire("Vlad")
+    println(vlad)
+    vlad.takeDamage(8)
+    println(vlad)
 
-    val axe = Weapon("Axe", 12)
+    val dracula = VampireKing("Dracula")
+    println(dracula)
 
-    tim.weapon = Weapon("Sword", damageInflicted = 20)
-    louise.weapon = tim.weapon
-    tim.weapon = Weapon("Spear", damageInflicted = 15)
+    while (dracula.lives > 0) {
+        if (dracula.dodges()){
+            continue
+        }
 
-    eoghan.weapon = axe
-
-    val redPotion = Loot("RedPotion", LootType.POTION, 7.50)
-    tim.getLoot(redPotion)
-    val chestArmour = Loot( "+3 Chest Armour", LootType.ARMOUR, 80.00)
-    tim.getLoot(chestArmour)
-
-    tim.showInventory()
-
-    tim.getLoot(Loot("Ring of protection +2", LootType.RING, 40.25))
-
-    tim.getLoot(Loot("Invisibility", LootType.POTION, 35.95))
-    tim.showInventory()
-
-    if(tim.dropLoot(redPotion)){
-        tim.showInventory()
-    }else{
-        println("you dont have a ${redPotion.name}")
+        if (dracula.runAway()){
+            println("Dracula ran away")
+            break
+        }else{
+            dracula.takeDamage(12)
+        }
     }
-
-    val bluePotion = Loot("Blue Potion", LootType.POTION, 6.00)
-
-    if(tim.dropLoot(bluePotion)){
-        tim.showInventory()
-    }else{
-        println("You dont have ${bluePotion.name}")
-    }
-
-     tim.dropLoot("Invisibility Potion")
-
-    tim.showInventory()
-
 }
